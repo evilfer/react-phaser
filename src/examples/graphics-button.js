50023f4fea26b765c7dfb18b4664feef92134b93
Eloy Villasclaras <eloy.villasclara@gmail.com>
2016-04-18 00:42:08 +0100

    Implemented (initial) text align Including in buttons

--- src/examples/graphics-button.js
+++ src/examples/graphics-button.js
@@ -13,7 +13,9 @@ var React = require('../native'),
                             <rect x={0} y={120} width={200} height={40} fill={0xFFFF00}/>
                         </renderimage>
                     </assets>
-                    <button x={0} y={0} assetKey="button" frames={[0, 1, 2, 3]}/>
+                    <button x={0} y={0} assetKey="button" frames={[0, 1, 2, 3]}>
+                        <text align="center middle" text="hi!"/>
+                    </button>
                 </game>
             );
         }
--- src/impl/types/text.js
+++ src/impl/types/text.js
@@ -4,10 +4,19 @@ var treeUtils = require('../tree-utils'),
     textPropertes = require('../properties/base/Phaser.Text'),
 
     updateText = treeUtils.genPropertyMapUpdate(textPropertes),
-    
+
     initText = function (node, tree) {
-        var props = node.props;
-        node.obj = new Phaser.Text(treeUtils.game(tree), props.x, props.y, props.text, props.style);
+        var props = node.props,
+            text = new Phaser.Text(treeUtils.game(tree), 0, 0, props.text, props.style),
+            container = treeUtils.parent(node, tree).obj,
+            x = props.x || (props.align && (container.width - text.width) * (props.align.indexOf('right') >= 0 ? 1 :
+                    (props.align.indexOf('center') >= 0 ? .5 : 0))) || 0,
+            y = props.y || (props.align && (container.height - text.height) * (props.align.indexOf('bottom') >= 0 ? 1 :
+                    (props.align.indexOf('middle') >= 0 ? .5 : 0))) || 0;
+
+        text.x = x;
+        text.y = y;
+        node.obj = text;
         treeUtils.addDisplayObject(node, tree);
     },
 
