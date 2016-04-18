29a5294f465e44b178bee375d777826a68bfc9dc
Eloy Villasclaras <eloy.villasclara@gmail.com>
2016-04-18 22:34:02 +0100

    Added additional graphics options
    arc, ellipse, bezier curve, rounded rect

--- src/examples/graphics-button.js
+++ src/examples/graphics-button.js
@@ -1,5 +1,11 @@
 var React = require('../native'),
 
+    buttonShape = {
+        width: 200,
+        height: 40,
+        radius: 5
+    },
+
     MyGame = React.createClass({
 
         render: function () {
@@ -7,10 +13,10 @@ var React = require('../native'),
                 <game width={800} height={600}>
                     <assets>
                         <renderimage assetKey="button" frameHeight={40}>
-                            <rect x={0} y={0} width={200} height={40} fill={0x0000FF}/>
-                            <rect x={0} y={40} width={200} height={40} fill={0x00FF00}/>
-                            <rect x={0} y={80} width={200} height={40} fill={0xFF0000}/>
-                            <rect x={0} y={120} width={200} height={40} fill={0xFFFF00}/>
+                            <roundedrect x={0} y={0} {...buttonShape} fill={0x0000FF}/>
+                            <roundedrect x={0} y={40} {...buttonShape} fill={0x00FF00}/>
+                            <roundedrect x={0} y={80} {...buttonShape} fill={0xFF0000}/>
+                            <roundedrect x={0} y={120} {...buttonShape} fill={0xFFFF00}/>
                         </renderimage>
                     </assets>
                     <button x={0} y={0} assetKey="button" frames={[0, 1, 2, 3]}>
--- src/impl/types/graphics/renderers.js
+++ src/impl/types/graphics/renderers.js
@@ -1,11 +1,30 @@
 var createGraphicsNode = require('./create-graphics-item'),
 
     renderers = {
+        arc: function (node, tree, graphics, x0, y0) {
+            graphics.arc(
+                x0 + (node.props.x || 0),
+                y0 + (node.props.y || 0),
+                node.props.radius || 0,
+                node.props.startAngle || 0,
+                node.props.endAngle || 2 * Math.PI,
+                node.props.anticlockwise || false,
+                node.props.segments
+            );
+        },
         circle: function (node, tree, graphics, x0, y0) {
             graphics.drawCircle(
                 x0 + (node.props.x || 0),
                 y0 + (node.props.y || 0),
                 node.props.diameter || 0
+            );
+        },
+        ellipse: function (node, tree, graphics, x0, y0) {
+            graphics.drawEllipse(
+                x0 + (node.props.x || 0),
+                y0 + (node.props.y || 0),
+                node.props.width || 0,
+                node.props.height || 0
             );
         },
         rect: function (node, tree, graphics, x0, y0) {
@@ -14,6 +33,15 @@ var createGraphicsNode = require('./create-graphics-item'),
                 y0 + (node.props.y || 0),
                 node.props.width || 0,
                 node.props.height || 0
+            );
+        },
+        roundedrect: function (node, tree, graphics, x0, y0) {
+            graphics.drawRoundedRect(
+                x0 + (node.props.x || 0),
+                y0 + (node.props.y || 0),
+                node.props.width || 0,
+                node.props.height || 0,
+                node.props.radius || 0
             );
         },
         line: function (node, tree, graphics, x0, y0) {
@@ -32,10 +60,20 @@ var createGraphicsNode = require('./create-graphics-item'),
                 y0 + (node.props.y || 0)
             );
         },
-        curveto: function (node, tree, graphics, x0, y0) {
+        quadraticcurveto: function (node, tree, graphics, x0, y0) {
             graphics.quadraticCurveTo(
                 node.props.cpx + x0,
                 node.props.cpy + y0,
+                node.props.x + x0,
+                node.props.y + y0
+            );
+        },
+        beziercurveto: function (node, tree, graphics, x0, y0) {
+            graphics.quadraticCurveTo(
+                node.props.cpx + x0,
+                node.props.cpy + y0,
+                node.props.cpx2 + x0,
+                node.props.cpy2 + y0,
                 node.props.x + x0,
                 node.props.y + y0
             );
@@ -54,20 +92,32 @@ var createGraphicsNode = require('./create-graphics-item'),
                         });
 
                     switch (command) {
+                        case 'a':
+                            graphics.arc(v[0] + sx0, v[1] + sy0, v[2], v[3], v[4], !!v[5], v[6]);
+                            break;
                         case 'l':
                             graphics.lineTo(v[0] + sx0, v[1] + sy0);
                             break;
                         case 'c':
                             graphics.drawCircle(v[0] + sx0, v[1] + sy0, v[2]);
                             break;
+                        case 'e':
+                            graphics.drawEllipse(v[0] + sx0, v[1] + sy0, v[2], v[3]);
+                            break;
                         case 'r':
                             graphics.drawRect(v[0] + sx0, v[1] + sy0, v[2], v[3]);
+                            break;
+                        case 'd':
+                            graphics.drawRoundedRect(v[0] + sx0, v[1] + sy0, v[2], v[3], v[4]);
                             break;
                         case 'm':
                             graphics.moveTo(v[0] + sx0, v[1] + sy0);
                             break;
+                        case 'b':
+                            graphics.bezierCurveTo(v[0] + sx0, v[1] + sy0, v[2] + sx0, v[3] + sy0, v[4] + sx0, v[5] + sy0);
+                            break;
                         case 'q':
-                            graphics.quadraticCurveTo(v[0], v[1], v[2] + sx0, v[3] + sy0);
+                            graphics.quadraticCurveTo(v[0] + sx0, v[1] + sy0, v[2] + sx0, v[3] + sy0);
                             break;
                     }
                 }
